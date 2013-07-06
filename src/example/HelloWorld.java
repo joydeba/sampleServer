221ac26a028e769cb4e4ba5e633a1408f3db8b30
joydeba <joydeba@yahoo.com>
2013-07-07 02:29:43 +0600

    first

---
+++ src/example/HelloWorld.java
@@ -0,0 +1,14 @@
+package example;/**
+ * Created with IntelliJ IDEA.
+ * User: joy
+ * Date: 7/7/13
+ * Time: 1:11 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+  public String sayHelloWorldFrom(String from) {
+    String result = "Hello, world, from " + from;
+    System.out.println(result);
+    return result;
+  }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: joy
+  Date: 7/7/13
+  Time: 1:11 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
